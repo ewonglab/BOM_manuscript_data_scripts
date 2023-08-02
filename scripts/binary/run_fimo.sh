0033455a1bfae817998372c23f0ebc2bc810b5f5
XuanZhang <xzhangxmu@gmail.com>
2023-08-02 13:10:01 +1000

    Update run_fimo.sh

--- scripts/binary/run_fimo.sh
+++ scripts/binary/run_fimo.sh
@@ -1,23 +1,48 @@
 #!/bin/bash
 
-output_dir=$1
-shift
+O=/g/data/zk16/xzhang/BOM/Tutorial/motifs
+M=/g/data/zk16/useful/gimmemotifs/gimme.vertebrate.v5.0.meme
+G=/g/data/zk16/cc3704/mouse_data/Mus_musculus.GRCm38.dna_sm.primary_assembly.fa
+B=/g/data/zk16/xzhang/BOM/Tutorial/bed_files
 
-genome_path=$1
-shift
+mkdir -p ${O}
 
-mkdir -p "$output_dir"
-# Set path to motif database
-motifsDB='/path/to/gimme.vertebrate.v5.0.meme'
-
-for bed_file in "$@"
+while getopts o:g:b:m: op
+do 
+    case $op in
+        o)
+            echo "Output file folder is: $OPTARG"
+            O=$OPTARG;;
+        g)
+            echo "Path to genome reference fasta file is: $OPTARG"
+            G=$OPTARG;;
+        b)
+            echo "Path to bed files: $OPTARG"
+            B=$OPTARG;;
+        m)
+            echo "Path to motif database: $OPTARG"
+            M=$OPTARG;;
+        \?)
+            echo "Usage: args [-o] [-g] [-b] [-m] "
+            echo "-m means path to motif database"
+            echo "-g means path to genome reference fasta file"
+            echo "-o set a output path (Default: ./Tutorial/motifs) "
+            echo "-b means path to bed file folder (Default: ./Tutorial/bed_files)"
+            exit 1;;
+    esac
+done
+  
+  
+for bed_file in ${B}/*
 do
     fa_file="${bed_file%.bed}.fa"
-    bedtools getfasta -fi ${genome_path} -bed ${bed_file} -fo ${fa_file}
     filename=$(basename "$bed_file")
     extension="${filename##*.}"
     filename="${filename%.*}"
-
-    fimo --thresh 0.0001 --o "$output_dir/$filename" "$motifsDB" "$fa_file"
+    
+    # get DNA sequence from bed files
+    bedtools getfasta -fi ${G} -bed ${bed_file} -fo ${fa_file}
+    
+    # use fimo to search motifs
+    fimo --thresh 0.0001 --o ${O}/${filename} ${M} ${fa_file}
 done
-
