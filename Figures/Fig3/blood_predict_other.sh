4abc187d980b627d24865a700bbb61b5d8034d1c
Paola Cornejo <37220709+cosmiccapybara@users.noreply.github.com>
2025-09-18 00:00:48 +1000

    Create blood_predict_other.sh

---
+++ Figures/Fig3/blood_predict_other.sh
@@ -0,0 +1,16 @@
+declare -a model_li=("B" "CMP.LMPP" "CD14.Mono.1" "Early.Baso" "CD14.Mono.2" "Early.Eryth" "CD4.M" "GMP" "CD4.N1"
+                     "GMP.Neut" "CD4.N2" "HSC" "CD8.CM" "Late.Eryth" "CD8.EM" "NK" "CD8.N" "pDC" "cDC"
+                     "Plasma" "CLP.1" "Pre.B" "CLP.2" "Unk_26")
+declare -a target_li=("B" "CMP.LMPP" "CD14.Mono.1" "Early.Baso" "CD14.Mono.2" "Early.Eryth" "CD4.M" "GMP" "CD4.N1"
+                      "GMP.Neut" "CD4.N2" "HSC" "CD8.CM" "Late.Eryth" "CD8.EM" "NK" "CD8.N" "pDC" "cDC"
+                      "Plasma" "CLP.1" "Pre.B" "CLP.2" "Unk_26")
+
+for model in ${!model_li[@]}  
+do
+for target in ${!target_li[@]}
+do
+if [[ "${model_li[$model]}" != "${target_li[$target]}" ]]; then
+Rscript predict_other.R  "${model_li[$model]}" "${target_li[$target]}"
+fi
+done
+done
